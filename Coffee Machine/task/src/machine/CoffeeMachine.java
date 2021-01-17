055466a8929f283b1bfdeddbc1bcb3266c925d22
Damianos Sotirakis <ctl_doliveira@uolinc.com>
2021-01-17 17:27:28 -0300

    refact(01) : refactor structure

--- Coffee Machine/task/src/machine/CoffeeMachine.java
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -1,14 +1,12 @@
 package machine;
 
+import machine.controller.CoffeeMachineController;
+
 import java.util.Scanner;
 
 public class CoffeeMachine {
-    static int WATTER = 200;
-    static int MILK = 50;
-    static int COFFEE = 15;
-
-
     public static void main(String[] args){
+        CoffeeMachineController controller = new CoffeeMachineController();
         Scanner in = new Scanner(System.in);
         System.out.print("Write how many ml of water the coffee machine has");
         int watterDisposable = in.nextInt();
@@ -18,18 +16,18 @@ public class CoffeeMachine {
         int coffeeDisposable = in.nextInt();
         System.out.println("Write how many cups of coffee you will need:");
         int cupsRequired = in.nextInt();
-        int received = isPossible(cupsRequired, watterDisposable, milkDisposable,coffeeDisposable);
+        int received = controller.findByStatus(cupsRequired, watterDisposable, milkDisposable,coffeeDisposable);
         switch (received){
             case 0:
-                System.out.println("No, I can make only " + possibilities(watterDisposable, milkDisposable,coffeeDisposable) + " cup(s) of coffee");
+                System.out.println("No, I can make only " + controller.findByCapacity(watterDisposable, milkDisposable,coffeeDisposable) + " cup(s) of coffee");
                 break;
             case 2:
                 System.out.println("Yes, I can make that amount of coffee");
                 break;
             case 3:
-                if(surplus(cupsRequired, watterDisposable, milkDisposable,coffeeDisposable) > 0){
+                if(controller.surplus(cupsRequired, watterDisposable, milkDisposable,coffeeDisposable) > 0){
                     System.out.println("Yes, I can make that amount of coffee (and even "
-                            +surplus(cupsRequired, watterDisposable, milkDisposable,coffeeDisposable)+" more than that)");
+                            + controller.surplus(cupsRequired, watterDisposable, milkDisposable,coffeeDisposable)+" more than that)");
                     break;
                 }
                 System.out.println("Yes, I can make that amount of coffee");
@@ -39,81 +37,4 @@ public class CoffeeMachine {
                 break;
         }
     }
-
-    public static int isPossible(int cups, int water, int milk , int coffee){
-        if(have(cups, water, 1) || have(cups, milk, 2) || have(cups, coffee, 0)){
-            return 0;
-        }else if(left(cups, water, 1) && left(cups, milk, 2) && left(cups, coffee, 0)){
-            return 3;
-        }else{
-            return 2;
-        }
-    }
-
-    public static boolean have(int cups, int supply, int type){
-        switch (type){
-            case 1:
-                return (cups * WATTER) - supply > 0;
-            case 2:
-                return (cups * MILK) - supply > 0;
-            default:
-                return (cups * COFFEE) - supply > 0;
-        }
-    }
-
-    public static boolean left(int cups, int supply, int type){
-        switch (type){
-            case 1:
-                return supply - (cups * WATTER)  > 0;
-            case 2:
-                return supply - (cups * MILK) > 0;
-            default:
-                return supply - (cups * COFFEE) > 0;
-
-        }
-    }
-
-    public static int possibilities(int water, int milk , int coffee){
-        int waterPossible = 0;
-        int milkPossible = 0;
-        int coffeePossible = 0;
-        for (int i = WATTER; i <= water; i += WATTER){
-            waterPossible++;
-        }
-        for (int i = MILK; i <= milk; i += MILK){
-            milkPossible++;
-        }
-        for (int i = COFFEE; i <= coffee; i += COFFEE){
-            coffeePossible++;
-        }
-        return hasLess(waterPossible, milkPossible, coffeePossible);
-    }
-
-    public static int surplus(double cups, double water, double milk , double coffee){
-        return hasLess((water - (cups * WATTER)) / WATTER,
-                (milk - (cups * MILK)) / MILK,
-                (coffee - (cups * COFFEE)) / COFFEE);
-    }
-
-    public static int hasLess(double water, double milk , double coffee){
-        double[] values = new double[3];
-        values[0] = water;
-        values[1] = milk;
-        values[2] = coffee;
-        double minimum = Double.MIN_VALUE;
-        double maximo = Double.MAX_VALUE;
-        for (Double value : values) {
-            if (minimum <= value && value > 0) {
-                minimum = value;
-            }
-        }
-        for (Double value : values) {
-            if (maximo >= value && value > 0) {
-                maximo = value;
-            }
-        }
-        return Math.min((int) minimum, (int) maximo);
-    }
-
-
 }
---
+++ Coffee Machine/task/src/machine/service/CoffeeMachineService.java
@@ -0,0 +1,84 @@
+package machine.service;
+
+import machine.model.enumerated.Supply;
+
+public class CoffeeMachineService {
+
+    public int isPossible(int cups, int water, int milk , int coffee){
+        if(have(cups, water, 1) || have(cups, milk, 2) || have(cups, coffee, 0)){
+            return 0;
+        }else if(left(cups, water, 1) && left(cups, milk, 2) && left(cups, coffee, 0)){
+            return 3;
+        }else{
+            return 2;
+        }
+    }
+
+    public boolean have(int cups, int item, int type){
+
+        switch (type){
+            case 1:
+                return (cups * Supply.WATTER.getDefaultValue()) - item > 0;
+            case 2:
+                return (cups * Supply.MILK.getDefaultValue()) - item > 0;
+            default:
+                return (cups * Supply.COFFEE.getDefaultValue()) - item > 0;
+        }
+    }
+
+    public boolean left(int cups, int item, int type){
+        switch (type){
+            case 1:
+                return item - (cups * Supply.WATTER.getDefaultValue())  > 0;
+            case 2:
+                return item - (cups * Supply.MILK.getDefaultValue()) > 0;
+            default:
+                return item - (cups * Supply.COFFEE.getDefaultValue()) > 0;
+
+        }
+    }
+
+    public int possibilities(int water, int milk , int coffee){
+
+        int waterPossible = 0;
+        int milkPossible = 0;
+        int coffeePossible = 0;
+        for (int i = Supply.WATTER.getDefaultValue(); i <= water; i += Supply.WATTER.getDefaultValue()){
+            waterPossible++;
+        }
+        for (int i = Supply.MILK.getDefaultValue(); i <= milk; i += Supply.MILK.getDefaultValue()){
+            milkPossible++;
+        }
+        for (int i = Supply.COFFEE.getDefaultValue(); i <= coffee; i += Supply.COFFEE.getDefaultValue()){
+            coffeePossible++;
+        }
+        return hasLess(waterPossible, milkPossible, coffeePossible);
+    }
+
+    public int surplus(double cups, double water, double milk , double coffee){
+
+        return hasLess((water - (cups * Supply.WATTER.getDefaultValue())) / Supply.WATTER.getDefaultValue(),
+                (milk - (cups * Supply.MILK.getDefaultValue())) / cups * Supply.MILK.getDefaultValue(),
+                (coffee - (cups * Supply.COFFEE.getDefaultValue())) / Supply.COFFEE.getDefaultValue());
+    }
+
+    public int hasLess(double water, double milk , double coffee){
+        double[] values = new double[3];
+        values[0] = water;
+        values[1] = milk;
+        values[2] = coffee;
+        double minimum = Double.MIN_VALUE;
+        double maximo = Double.MAX_VALUE;
+        for (Double value : values) {
+            if (minimum <= value && value > 0) {
+                minimum = value;
+            }
+        }
+        for (Double value : values) {
+            if (maximo >= value && value > 0) {
+                maximo = value;
+            }
+        }
+        return Math.min((int) minimum, (int) maximo);
+    }
+}
