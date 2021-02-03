bcfab789296cd1b6b351cad7dab50313a0e4010c
Damianos Sotirakis <ctl_doliveira@uolinc.com>
2021-02-03 01:12:50 -0300

    feat(04) : pass step 4/5 this machine ready with rest structure

--- Coffee Machine/task/src/machine/service/MachineService.java
+++ Coffee Machine/task/src/machine/service/MachineService.java
@@ -2,25 +2,47 @@ package machine.service;
 
 import machine.model.Machine;
 import machine.repository.MachineRepository;
+import java.util.Scanner;
 
 public class MachineService {
 
+    private static final String ITEM_1 = "Write how many ml of water do you want to add";
+    private static final String ITEM_2 = "Write how many ml of milk do you want to add";
+    private static final String ITEM_3 = "Write how many grams of coffee beans do you want to add";
+    private static final String ITEM_4 = "Write how many disposable cups of coffee do you want to add";
+
+    Scanner in = new Scanner(System.in);
     MachineRepository repository = new MachineRepository();
 
     public void buy(Machine session) {
         try {
             session.validate(session);
+            repository.pay(session);
         } catch (Exception e){
-            System.out.println("Error : " + e.getMessage()+ "\n Causa : " + e.getCause());
+            System.out.println("Error : " + e.getMessage()+"\nCausa : " + e.getCause());
         }
-        repository.pay(session);
     }
 
-    public void take(Machine session) {
-
+    public Machine take(Machine session) {
+        System.out.println("I gave you $" + session.getMoney());
+        session.setMoney(0);
+        return session;
     }
 
-    public void fill(Machine session) {
+    public Machine fill(Machine session) {
 
+        System.out.println(ITEM_1);
+        session.setWatter(in.nextInt()+ session.getWatter());
+
+        System.out.println(ITEM_2);
+        session.setMilk(in.nextInt()+ session.getMilk());
+
+        System.out.println(ITEM_3);
+        session.setCoffee(in.nextInt()+ session.getCoffee());
+
+        System.out.println(ITEM_4);
+        session.setCup(in.nextInt()+ session.getCup());
+
+        return session;
     }
 }
