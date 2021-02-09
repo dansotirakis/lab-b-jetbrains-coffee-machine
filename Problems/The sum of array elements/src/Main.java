0ee95ea094d106dea286cd6f60cda1a1c1dd8955
Damianos Sotirakis <ctl_doliveira@uolinc.com>
2021-02-09 10:03:28 -0300

    feat(ARY02) : the sum of array elements finally

--- Problems/The sum of array elements/src/Main.java
+++ Problems/The sum of array elements/src/Main.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
-        // put your code here
+        Scanner scanner = new Scanner(System.in);
+        int result = 0;
+        for (int i = scanner.nextInt(); i >= 1; i--) {
+            result += Integer.parseInt(scanner.next());
+        }
+        System.out.println(result);
     }
 }
